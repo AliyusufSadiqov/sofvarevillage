d14d0659fce97d627acc0503d066c09748a64085
Sadiqov Eliyusif <alisadiqov20002@gmail.com>
2024-10-06 17:27:57 +0400

    Initial commit

---
+++ src/democode3/Demo.java
@@ -0,0 +1,16 @@
+package democode3;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        a=b-a;
+        b=b-a;
+        a=a+b;
+        System.out.println(a+" "+b);
+    }
+}
---
+++ src/democode5/DemoCode2.java
@@ -0,0 +1,7 @@
+package democode5;
+
+public class DemoCode2 {
+    public static void main(String[] args) {
+
+    }
+}
